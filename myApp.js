efb28c05c6d8b60e2f8dac787a41e8e890fb633c
GautamR24 <rawatgautam24@protonmail.com>
2024-01-09 23:16:48 +0530

    update

--- myApp.js
+++ myApp.js
@@ -2,11 +2,17 @@
 let express = require("express");
 let app = express();
 //adding first function
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   var absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
 
 //mounting the middleware
 app.use("/public", express.static(__dirname + "/public"));
+
+//creating the json route
+
+app.get('/json', function(res) {
+  res.json({ message: "Hello json" });
+})
 module.exports = app;
