dfab15327d37c299210b47bcdfe32c7107224353
GautamR24 <rawatgautam24@protonmail.com>
2024-01-09 23:01:06 +0530

    update

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@
 let express = require('express');
 let app = express();
 
+//mounting the middleware
+app.use('/public', function(req, res) {
+  var absolutePath = __dirname + '/public';
+  express.static(absolutePath);
+})
 //adding first function
 app.get('/', function(req, res) {
   var absolutePath = __dirname + '/views/index.html'
